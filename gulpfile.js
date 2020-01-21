b8c1f6a690aa105a4d493cfa46bbdd80207f988b
Arseniy Rybalov <mrsinkrait@gmail.com>
2020-01-21 22:25:34 +0600

    header commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+
+
+var concatCss = require('gulp-concat-css');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let watch = require('gulp-watch');
+
+gulp.task('sass-compile',function(){
+    return gulp.src('styles/sass/*.sass')
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error',sass.logError))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('styles/css'))
+    });
+
+    gulp.task('watch',function(){
+    gulp.watch('styles/sass/*.sass',gulp.series('sass-compile','con-min'));
+    });
+
+gulp.task('con-min', function () {
+  return gulp.src('styles/css/*.css')
+    .pipe(concatCss("styles/all.css"))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest('importCss/'));
+});
+
+
+
+
+ 
